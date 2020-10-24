d4507ccd291607388403da88db8e85092b61751c
devtinatina <53737471+devtinatina@users.noreply.github.com>
2020-10-24 17:06:40 +0900

    update n1001, n1008, n10998

---
+++ n1008/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        double A, B, C;
+
+        A = scan.nextDouble();
+        B = scan.nextDouble();
+        C = A / B;
+        System.out.println(C);
+
+    }
+}
